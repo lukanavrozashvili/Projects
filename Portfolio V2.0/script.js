31134d269e4caedb437c0788258eed1c17622ab2
Luka Navrozashvili <143267861+lukanavrozashvili@users.noreply.github.com>
2024-08-08 23:30:42 +0400

    Add files via upload

---
+++ Portfolio V2.0/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".auto-type",{
+    strings: ["Front-End Developer", "UI/UX Designer", "Back-End Developer"],
+    typeSpeed: 120,
+    backSpeed: 120,
+    loop: true
+})
+
+AOS.init();
