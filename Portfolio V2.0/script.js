21037abadf1c562efe9072ac9a02528efd73d552
Luka Navrozashvili <143267861+lukanavrozashvili@users.noreply.github.com>
2024-08-08 23:49:20 +0400

    Delete Portfolio V2.0 directory

--- Portfolio V2.0/script.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".auto-type",{
-    strings: ["Front-End Developer", "UI/UX Designer", "Back-End Developer"],
-    typeSpeed: 120,
-    backSpeed: 120,
-    loop: true
-})
-
-AOS.init();
